c786813510316f2f40874496c8b52fd155ed8624
Tom <thomasaeh1@gmail.com>
2021-08-11 10:21:59 +0100

    got hello world api working for backend

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Where we will keep books
+let books = [];
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+    res.send('hello world')
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
