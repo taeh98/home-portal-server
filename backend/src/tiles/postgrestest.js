90769617652f1376b98ec7a00cf36734cdad4091
Tom Hardern <thomasaeh1@gmail.com>
2021-08-12 20:15:26 +0100

    got postgrestest.js working with sequelize for strictly typed queries

--- backend/src/tiles/postgrestest.js
+++ backend/src/tiles/postgrestest.js
@@ -1,24 +1,56 @@
-const { Pool, Client } = require('pg');
+const { Sequelize, DataTypes } = require('sequelize');
 
 console.log("in postgrestest.js");
 
-const pool = new Pool({
-    user: process.env.postgres_tiles_db_user_name,
-    host: process.env.postgres_tiles_db_container_name,
-    database: process.env.postgres_tiles_db_name,
-    password: process.env.postgres_tiles_db_user_password,
-    port: process.env.postgres_tiles_db_port,
+const postgresUri = 'postgres://' + process.env.postgres_tiles_db_user_name + ':' + process.env.postgres_tiles_db_user_password +
+    '@' + process.env.postgres_tiles_db_container_name + ':' + process.env.postgres_tiles_db_port + '/' + process.env.postgres_tiles_db_name;
+
+const sequelize = new Sequelize(postgresUri); // Example for postgres
+
+console.debug("postgresUri = ", postgresUri);
+
+const Link = sequelize.define('Link', {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    url: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    clickCount: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: "click_count"
+    },
+}, {
+    tableName: "links",
+    timestamps: false
 });
 
 async function main() {
     console.log("in main() in postgrestest.js");
 
-    pool.query('SELECT * from links;', (err, res) => {
-        console.log(err, res);
-    });
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+
+        const syncRes = await Link.sync();
+
+        console.debug("syncRes = ", syncRes);
+
+        const findAllRes = await Link.findAll();
+
+        console.debug("findAllRes = ", findAllRes);
+
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
 }
 
-main()
-    .then(console.log)
-    .catch(console.error)
-    .finally(() => client.close());
+main();
