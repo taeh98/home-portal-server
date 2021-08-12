f5877b6447fe94a015ad53916ac604d670755ab7
Tom Hardern <thomasaeh1@gmail.com>
2021-08-12 15:04:02 +0100

    fixed error running make_links_postgres_table.sql by using single quotes for strings instead of single quotes (used for ids)

--- make_links_postgres_table.sql
+++ make_links_postgres_table.sql
@@ -7,14 +7,14 @@ CREATE TABLE IF NOT EXISTS links (
 
 INSERT INTO links (name , url, click_count)
 VALUES
-    ("Google", "https://www.google.com", 0),
-    ("YouTube", "https://www.youtube.com", 0),
-    ("Facebook", "https://facebook.com", 0),
-    ("Twitter", "https://twitter.com", 0),
-    ("Spotify", "https://www.spotify.com", 0),
-    ("Netflix", "https://www.netflix.com", 0),
-    ("Wikipedia", "https://www.wikipedia.org", 0),
-    ("Reuters", "https://www.reuters.com", 0),
-    ("Associated Press", "https://apnews.com", 0),
-    ("CNBC", "https://www.cnbc.com/", 0),
-    ("AccuWeather", "https://www.accuweather.com", 0);
+    ('Google', 'https://www.google.com', 0),
+    ('YouTube', 'https://www.youtube.com', 0),
+    ('Facebook', 'https://facebook.com', 0),
+    ('Twitter', 'https://twitter.com', 0),
+    ('Spotify', 'https://www.spotify.com', 0),
+    ('Netflix', 'https://www.netflix.com', 0),
+    ('Wikipedia', 'https://www.wikipedia.org', 0),
+    ('Reuters', 'https://www.reuters.com', 0),
+    ('Associated Press', 'https://apnews.com', 0),
+    ('CNBC', 'https://www.cnbc.com', 0),
+    ('AccuWeather', 'https://www.accuweather.com', 0);
