c4016033d95b837bb012a06ae031b2c239eadb1f
Tom Hardern <thomasaeh1@gmail.com>
2021-08-13 07:27:37 +0100

    adjusted postgrestest.js to reflect tiles.ts

--- backend/src/tiles/postgrestest.js
+++ backend/src/tiles/postgrestest.js
@@ -1,15 +1,11 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-console.log("in postgrestest.js");
-
 const postgresUri = 'postgres://' + process.env.postgres_tiles_db_user_name + ':' + process.env.postgres_tiles_db_user_password +
     '@' + process.env.postgres_tiles_db_container_name + ':' + process.env.postgres_tiles_db_port + '/' + process.env.postgres_tiles_db_name;
 
-const sequelize = new Sequelize(postgresUri); // Example for postgres
+const sequelize = new Sequelize(postgresUri);
 
-console.debug("postgresUri = ", postgresUri);
-
-const Link = sequelize.define('Link', {
+const linkModel = sequelize.define('Link', {
     id: {
         type: DataTypes.INTEGER,
         allowNull: false,
@@ -33,24 +29,31 @@ const Link = sequelize.define('Link', {
     timestamps: false
 });
 
-async function main() {
-    console.log("in main() in postgrestest.js");
-
+async function getAllTiles() {
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
 
-        const syncRes = await Link.sync();
+        const syncRes = await linkModel.sync();
 
         console.debug("syncRes = ", syncRes);
 
-        const findAllRes = await Link.findAll();
+        const findAllRes = await linkModel.findAll();
 
         console.debug("findAllRes = ", findAllRes);
 
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
+        return findAllRes.map((resItem) => ({
+            id: resItem.getDataValue("id"),
+            name: resItem.getDataValue("name"),
+            url: resItem.getDataValue("url"),
+            clickCount: resItem.getDataValue("clickCount")
+        }));
+    }
+
+    catch (error) {
+        console.error('Unable to get tiles from the postgres database:', error);
+        return [];
     }
 }
 
-main();
+getAllTiles().then((res) => console.log("got tiles: ", res));
