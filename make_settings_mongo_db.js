6fb8382d7b6faae45d217f2d9c259320c7600606
Tom Hardern <thomasaeh1@gmail.com>
2021-08-15 11:25:50 +0100

    reformatted make_settings_mongo_db.js

--- make_settings_mongo_db.js
+++ make_settings_mongo_db.js
@@ -4,22 +4,27 @@ db = db.getSiblingDB('settings');
 
 collections = db.getCollectionNames();
 
-if (! collections.includes('appearance')) {
+if (!collections.includes('appearance')) {
     db.createCollection('appearance');
     db.appearance.insertMany([{backgroundColor: null, theme: "light"}]);
 }
 
-if (! collections.includes('news')) {
+if (!collections.includes('news')) {
     db.createCollection('news');
-    db.news.insertMany([{apiKey: null, sources: ["al-jazeera-english", "associated-press", "bbc-news", "reuters"], country: "gb", categories: null}]);
+    db.news.insertMany([{
+        apiKey: null,
+        sources: ["al-jazeera-english", "associated-press", "bbc-news", "reuters"],
+        country: "gb",
+        categories: null
+    }]);
 }
 
-if (! collections.includes('weather')) {
-    db.createCollection('news');
+if (!collections.includes('weather')) {
+    db.createCollection('weather');
     db.weather.insertMany([{apiKey: null, location: null}]);
 }
 
-if (! collections.includes('search')) {
+if (!collections.includes('search')) {
     db.createCollection('search');
     db.search.insertMany([{provider: "DuckDuckGo"}]);
 }
