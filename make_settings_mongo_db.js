42a0ba284d79b11efee105ad8e6ef43f2308a123
Tom Hardern <thomasaeh1@gmail.com>
2021-08-15 12:10:31 +0100

    started trying to debug values inserted into collections at enf of make_settings_mongo_db.js script

--- make_settings_mongo_db.js
+++ make_settings_mongo_db.js
@@ -4,6 +4,8 @@ db = db.getSiblingDB('settings');
 
 collections = db.getCollectionNames();
 
+print("initial collections = " + JSON.stringify(collections));
+
 if (!collections.includes('appearance')) {
     let appearanceRes = [
         db.createCollection('appearance'),
@@ -39,6 +41,15 @@ if (!collections.includes('search')) {
         db.search.insertMany([{provider: "DuckDuckGo"}])
     ];
     print("searchRes = " + JSON.stringify(searchRes));
+}
+
+collections = db.getCollectionNames();
+
+for (let i = 0; i < collections.length; i++) {
+    collection_name = collections[i];
+    collection = db.getCollection(collection_name);
+    indices = collection.getIndexes();
+    print("for i = " + i + ", collection_name = " + collection_name + ", and collection = " + collection + ", indices = " + JSON.stringify(indices));
 }
 
 if (error) {
