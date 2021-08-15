5d8978e91a34e00e98e0d51c5065371c51dbd48b
Tom Hardern <thomasaeh1@gmail.com>
2021-08-15 11:35:20 +0100

    added debugging results of ops on each collection to make_settings_mongo_db.js

--- make_settings_mongo_db.js
+++ make_settings_mongo_db.js
@@ -5,28 +5,40 @@ db = db.getSiblingDB('settings');
 collections = db.getCollectionNames();
 
 if (!collections.includes('appearance')) {
-    db.createCollection('appearance');
-    db.appearance.insertMany([{backgroundColor: null, theme: "light"}]);
+    let appearanceRes = [
+        db.createCollection('appearance'),
+        db.appearance.insertMany([{backgroundColor: null, theme: "light"}])
+    ];
+    print("appearanceRes = " + JSON.stringify(appearanceRes));
 }
 
 if (!collections.includes('news')) {
-    db.createCollection('news');
-    db.news.insertMany([{
-        apiKey: null,
-        sources: ["al-jazeera-english", "associated-press", "bbc-news", "reuters"],
-        country: "gb",
-        categories: null
-    }]);
+    let newsRes = [
+        db.createCollection('news'),
+        db.news.insertMany([{
+            apiKey: null,
+            sources: ["al-jazeera-english", "associated-press", "bbc-news", "reuters"],
+            country: "gb",
+            categories: null
+        }])
+    ];
+    print("newsRes = " + JSON.stringify(newsRes));
 }
 
 if (!collections.includes('weather')) {
-    db.createCollection('weather');
-    db.weather.insertMany([{apiKey: null, location: null}]);
+    let weatherRes = [
+        db.createCollection('weather'),
+        db.weather.insertMany([{apiKey: null, location: null}])
+    ];
+    print("weatherRes = " + JSON.stringify(weatherRes));
 }
 
 if (!collections.includes('search')) {
-    db.createCollection('search');
-    db.search.insertMany([{provider: "DuckDuckGo"}]);
+    let searchRes = [
+        db.createCollection('search'),
+        db.search.insertMany([{provider: "DuckDuckGo"}])
+    ];
+    print("searchRes = " + JSON.stringify(searchRes));
 }
 
 if (error) {
